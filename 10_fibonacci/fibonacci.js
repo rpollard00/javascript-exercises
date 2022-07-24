2377f4a5b8c634cc07b13ee03289e6703555e38d
Reese Pollard <rpollard@gmail.com>
2022-07-24 13:29:13 -0700

    Compute fibonacci sequence, return nth position

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
 
+    let f = [0, 1];
+
+    for (i = 2; i < parseInt(n)+1; i++) {
+        f.push(f[i-1] + f[i-2]);
+    }  // 0 1 1 2 3 5 8 13
+
+    return f[n];
 };
 
 // Do not edit below this line
