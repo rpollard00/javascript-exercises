58603b8278feeb6ede1454e6072bd3dd44158438
Reese Pollard <rpollard@gmail.com>
2022-07-24 13:29:52 -0700

    Write palindrome check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+    // strip non-alpha from string
+    alphaRegex = /[^a-z0-9]/gi;
+    const strClean = str.replace(alphaRegex, '').toLowerCase();
+
+    // reverse string
+    const strReverse = strClean.split('').reverse().join('')
+
+    // check forward vs backward 
+    return strClean === strReverse;
 };
 
 // Do not edit below this line
