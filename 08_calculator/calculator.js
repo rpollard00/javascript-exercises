bb31899ed4bd361f75c2bc9fb2005e4305ca00ae
Reese Pollard <rpollard@gmail.com>
2022-07-24 13:06:51 -0700

    Defined functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((total, val) => total += val,0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return add(...array);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, val) => total *= val,1);
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+  let raised = x;
+	for (i=1; i < n; i++) {
+    raised *= x;
+  }
+  return raised;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  let result = val;
+
+  if (val === 0) return 1;
+
+	for (i = val - 1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
